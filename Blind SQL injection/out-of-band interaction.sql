3f6be5d04dac0adf24ec832cc30428b08c05c77d
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-25 20:23:49 -0400

    Create out-of-band interaction.sql

---
+++ Blind SQL injection/out-of-band interaction.sql
@@ -0,0 +1,2 @@
+--The application uses a tracking cookie for analytics, and performs an SQL query containing the value of the submitted cookie.
+--The SQL query is executed asynchronously and has no effect on the application's response. However, you can trigger out-of-band interactions with an external domain. 
