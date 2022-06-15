2ab1ff5542f114352f6f14a340fbc15c6d69e4f3
HTN <96134288+htn-dev@users.noreply.github.com>
2022-06-15 17:39:22 -0400

    Update Time delays.sql

--- Blind SQL injection/Time delays.sql
+++ Blind SQL injection/Time delays.sql
@@ -1,6 +1,6 @@
 --Visit the front page of the shop, and use Burp Suite to intercept and modify the request containing the TrackingId cookie.
 --Modify the TrackingId cookie, changing it to:
-    
+     
 TrackingId=x'||pg_sleep(10)--
 
 '--Submit the request and observe that the application takes 10 seconds to respond.
