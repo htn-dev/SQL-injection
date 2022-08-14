793aa7b6bef3e0f4db78a1c2c1f1128a37d6af98
HTN <96134288+htn-dev@users.noreply.github.com>
2022-08-13 20:06:46 -0400

    Update Listing the contents of the database.sql

--- Examining the database/Listing the contents of the database.sql
+++ Examining the database/Listing the contents of the database.sql
@@ -1,6 +1,6 @@
 -- Most database types (with the notable exception of Oracle) have a set of views called the information schema which provide information about the database. --
 -- You can query information_schema.tables to list the tables in the database: --
-   
+    
 SELECT * FROM information_schema.tables
 
 --  You can then query information_schema.columns to list the columns in individual tables:
