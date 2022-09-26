a60e6bcae299b47fba2c0ec36e96a1a4431c445f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-09-26 15:45:23 -0400

    Update Out-of-band interaction.sql

--- Blind SQL injection/Out-of-band interaction.sql
+++ Blind SQL injection/Out-of-band interaction.sql
@@ -1,6 +1,6 @@
 --The application uses a tracking cookie for analytics, and performs an SQL query containing the value of the submitted cookie.
 --The SQL query is executed asynchronously and has no effect on the application's response. However, you can trigger out-of-band interactions with an external domain. 
-    
+     
 --Exploit the SQL injection vulnerability to cause a DNS lookup to Burp Collaborator.
 
 --Visit the front page of the shop, and use Burp Suite to intercept and modify the request containing the TrackingId cookie.
